260bfa5fb848ed36514b3f103aff56c3f2da3acd
Aftersol <42478428+Aftersol@users.noreply.github.com>
2026-03-21 18:11:51 -0700

    for converting raw n64 screenshots into qoi file

---
+++ tools/example_enc.c
@@ -0,0 +1,242 @@
+/*
+
+    -- example_enc.c -- Reference QOI encoding usage of this library
+
+    -- version 1.1 -- revised 2025-04-07
+
+    -- Changelog --
+
+    - version 1.1 (2025-04-07)
+        - Implemented error handling in case reading RGBA file fails
+        - Strictly check requested color channels and colorspace
+        according to QOI specifications
+
+    - version 1.0 (2025-01-13)
+
+    MIT License
+
+    Copyright (c) 2024-2025 Aftersol
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SIMPLIFIED_QOI_IMPLEMENTATION
+#include "sQOI.h"
+#include "conv_endian.h"
+
+void print_help()
+{
+    printf("Example usage: qoi_enc <filename> <width> <height> <channels> <colorspace> <output>\n");
+    printf("Channels:\n3: No transparency\n4: Transparency\n\n");
+    printf("Colorspace:\n0: sRGB with linear alpha\n1: Linear RGB\n");
+}
+
+int main(int argc, char* argv[])
+{
+
+    qoi_desc_t desc;
+    qoi_enc_t enc;
+    uint8_t* qoi_file, *pixel_seek, *file_buffer0, *file_buffer1;
+    FILE* fp;
+    uint32_t width, height;
+    uint8_t channels, colorspace;
+    size_t file_size;
+    
+    if (argc < 6)
+    {
+        print_help();
+        return -1;
+    }
+    else
+    {
+
+        if (strlen(argv[1]) <= 0)
+        {
+            print_help();
+            return -1;
+        }
+
+        width = strtoul(argv[2], NULL, 0);
+        height = strtoul(argv[3], NULL, 0);
+
+        /* Check if requested amount or color channels is 3 or 4 according to QOI specifications */
+        if (strtoul(argv[4], NULL, 0) >= 3 && strtoul(argv[4], NULL, 0) <= 4) {
+            channels = strtoul(argv[4], NULL, 0);
+        }
+        else
+        {
+            printf("Channels entered must be 3 (RGB) or 4 (RGBA)\n");
+            print_help();
+            return -1;
+        }
+
+        /* Check if requested colorspace is according to QOI specifications:
+           0 (sRGB with linear alpha)
+           1 (linear RGB)  
+        */
+        if (strtoul(argv[5], NULL, 0) >= 0 && strtoul(argv[5], NULL, 0) <= 1) {
+            colorspace = strtoul(argv[5], NULL, 0);
+        }
+        else
+        {
+            printf("Colorspace entered must be 0 (sRGB with linear alpha) or 1 (linear RGB)\n");
+            print_help();
+            return -1;
+        }
+
+        if (width < 0 || height < 0)
+        {
+            print_help();
+            return -1;
+        }
+    }
+
+    printf("Opening %s\n",argv[1]);
+
+    fp = fopen(argv[1], "rb");
+
+    if (!fp)
+    {
+        print_help();
+        return -1;
+    }
+
+    fseek(fp, 0, SEEK_END);
+    
+    file_size = ftell(fp);
+
+    if ((size_t)width * (size_t)height * (size_t)channels < file_size)
+    {
+        size_t image_size = (size_t)width * (size_t)height * (size_t)channels;
+        size_t size_difference = file_size - image_size;
+
+        printf(
+            "%zu bytes are required for the file, %s. Your requested dimensions and channels amount allocates %zu bytes. That is %zu %s difference\n",
+            file_size,
+            argv[1],
+            image_size,
+            size_difference,
+            /* for printing plurals from of the word "byte" */
+            (size_difference > 1) ? "bytes" : "byte"
+            );
+        print_help();
+
+        return -1;
+    }
+
+    fseek(fp, 0, SEEK_SET);
+
+    file_buffer0 = (uint8_t*)calloc(file_size + 1, sizeof(uint8_t));
+    file_buffer1 = (uint8_t*)calloc(file_size + 1, sizeof(uint8_t)*2);
+    if (!file_buffer0 || !file_buffer1)
+    {
+        fclose(fp);
+        return 1;
+    }
+
+    if (fread(file_buffer0, 1, file_size, fp) < file_size) 
+    {
+        if (ferror(fp)) 
+        {
+            printf("An error has occur while reading %s\n", argv[1]);
+            print_help();
+    
+            fclose(fp);
+            free(file_buffer0);
+    
+            return 1;
+        }
+    }
+
+    fclose(fp);
+    uint16_t* frame_buffer = (uint16_t*)file_buffer0;
+    for (size_t i = 0; i < file_size / sizeof(uint16_t); i++) {
+            
+            int r = (read_be_u16(frame_buffer[i]) >> 11) & 0x1F;
+            int g = (read_be_u16(frame_buffer[i]) >>  6) & 0x1F;
+            int b = (read_be_u16(frame_buffer[i]) >>  1) & 0x1F;
+            int a = (read_be_u16(frame_buffer[i]) >>  0) & 0x01;
+        
+            // expand to 8 bit
+            r = (r << 3) | (r >> 2);
+            g = (g << 3) | (g >> 2);
+            b = (b << 3) | (b >> 2);
+            a = a * 255;
+
+        if (channels == 4)
+        {
+            file_buffer1[i*4] = r;
+            file_buffer1[i*4+1] = g;
+            file_buffer1[i*4+2] = b;
+            file_buffer1[i*4+3] = a;
+        }
+        else {
+            file_buffer1[i*3] = r;
+            file_buffer1[i*3+1] = g;
+            file_buffer1[i*3+2] = b;
+        }
+
+    }
+
+    qoi_desc_init(&desc);
+    
+    qoi_set_dimensions(&desc, width, height);
+    qoi_set_channels(&desc, channels);
+    qoi_set_colorspace(&desc, colorspace);
+    
+    qoi_file = (uint8_t*)malloc(((size_t)desc.width * (size_t)desc.height * ((size_t)desc.channels + 1)) + 14 + 8 + sizeof(size_t));
+    
+    if (!qoi_file)
+    {
+        return 1;
+    }
+
+    printf("Encoding %s to %s. Please wait . . .\n", argv[1], argv[6]);
+
+    write_qoi_header(&desc, qoi_file);
+
+    pixel_seek = file_buffer1;
+
+    qoi_enc_init(&desc, &enc, qoi_file);
+
+    while(!qoi_enc_done(&enc))
+    {
+        qoi_encode_chunk(&desc, &enc, pixel_seek);
+
+        pixel_seek += desc.channels;
+    }
+
+    fp = fopen(argv[6], "wb");
+    
+    if (fp)
+    {
+        fwrite(qoi_file, 1, enc.offset - enc.data, fp);
+        fclose(fp);
+    } 
+
+    free(qoi_file);
+
+    return 0;
+}
